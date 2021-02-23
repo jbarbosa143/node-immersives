cb9ebd467bed36001e288f086bb58edca6c94c02
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-02-23 16:49:21 -0600

    donezies

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,12 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+
+const first = getInput(0);
+
+const last = getInput(1);
+
+console.log("--------------------");
+console.log("Hello" + first + " " + last +"! Thanks for using my app!");
+console.log("Let's scream your name! " + first.toUpperCase() + " " + last.toUpperCase());
+console.log("Now here are your initals! " + first[0] + "." + last[0] + ".");
+console.log("Finally here is your new email address! " + first.toLowerCase() + "." + last.toLowerCase() + "@codeimmersives.com")
